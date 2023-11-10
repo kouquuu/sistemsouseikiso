6fbee56fbc86ba642e742959b578713a76612b4f
kouquuu <148538280+kouquuu@users.noreply.github.com>
2023-11-10 19:13:08 +0900

    Create 1110.c

---
+++ 1110.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int max = 10000;
+
+	for (int i = 1; i <= max; i++)
+	{
+		if (i % 221 == 0)
+		{
+			printf("%d\n", i);
+		}
+		else if (i % 13 == 0)
+		{
+			printf("%d\n", i);
+		}
+		else if (i % 17 == 0)
+		{
+			printf("%d\n",i);
+		}
+		else
+		{}
+	}
+
+	return 0;
+}
